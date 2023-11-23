019e8fa185fbaacde79e062c162014dd9b87e86f
chinmayeeet <chinmayee.dindorez13@gmail.com>
2023-11-23 14:05:57 +0530

    DAL Layer

---
+++ DB/BookDal.cs
@@ -0,0 +1,110 @@
+﻿
+//using Microsoft.AspNetCore.Identity;
+//using ShoppingCart.Infrastructure.Validation;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace DB
+{
+
+    public class BookDal
+    {
+        [Key]
+        public int BookId { get; set; }
+
+        [StringLength(150)]
+        public string Title { get; set; }
+
+        [StringLength(150)]
+        public string Author { get; set; }
+
+        public float Price { get; set; }
+
+        [StringLength(500)]
+        public string Description { get; set; }
+
+        public DateTime PublicationDate { get; set; }
+
+        [StringLength(100)]
+        public string Publisher { get; set; }
+
+        //[ForeignKey]
+        public int CategoryId { get; set; }
+        public string Image { get; set; } = "noimage.png";
+
+        //[NotMapped]
+        //[FileExtension]
+        //public IFormFile ImageUpload { get; set; }
+    }
+    public class CartItem
+    {
+        public long BookId { get; set; }
+        public string BookName { get; set; }
+        public int Quantity { get; set; }
+        public float Price { get; set; }
+        public float Total
+        {
+            get { return (float)Quantity * Price; }
+        }
+        public string Image { get; set; }
+
+
+
+        public CartItem(BookDal book)
+        {
+            BookId = book.BookId;
+            BookName = book.Title;
+            Price = book.Price;
+            Quantity = 1;
+            Image = book.Image;
+        }
+
+
+    }
+    public class Category
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Slug { get; set; }
+    }
+
+    /*public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }*/
+
+    public class User
+    {
+        public string Id { get; set; }
+
+        [Required, MinLength(2, ErrorMessage = "Minimum length is 2")]
+        [Display(Name = "Username")]
+        public string UserName { get; set; }
+        [Required, EmailAddress]
+        public string Email { get; set; }
+        [DataType(DataType.Password), Required, MinLength(4, ErrorMessage = "Minimum length is 4")]
+        public string Password { get; set; }
+    }
+
+    
+
+    public class BookDbContext : DbContext
+    {
+        public DbSet<BookDal> Book{ get; set; }
+
+        public DbSet<User> User { get; set; }
+
+        //public DbSet<CartItem> Cart(BookDal) { get; set; }
+        public DbSet<Category> Category { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=EmployeeDb;Trusted_Connection=true");
+        }
+    }
+}
---
+++ DB/Migrations/20231123083015_v1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DB.Migrations
+{
+    public partial class v1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Book",
+                columns: table => new
+                {
+                    BookId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    Author = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    Price = table.Column<float>(type: "real", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    PublicationDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Publisher = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    CategoryId = table.Column<int>(type: "int", nullable: false),
+                    Image = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Book", x => x.BookId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Category",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Slug = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Category", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Book");
+
+            migrationBuilder.DropTable(
+                name: "Category");
+
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
