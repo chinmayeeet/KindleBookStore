a3602d98e5700e5d064289fd0a252ebf12cfcb0d
ssampreeth1 <sain2@kpmg.com>
2023-11-24 11:39:24 +0530

    Changes

---
+++ BStorePL/Models/CategoryModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BStorePL.Models
+{
+    public class CategoryModel
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Slug { get; set; }
+    }
+}
